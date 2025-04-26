8f7d8e451a72a74d2b06ccd78952e18716c03ce8
superSaiyanSkai <asuleodu@gmail.com>
2025-04-26 14:43:46 +0100

    made some changes i should have made a long time ago

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -14,18 +14,7 @@ const Auth = () => {
       <div className="flex overflow-hidden  justify-between w-[100%]  h-[100%]bg-black">
         <div className="lg:flex flex-col bg-black w-[50%] items-center justify-center hidden">
           <img className="w-[400px] " src={lion} alt="picture of thundercat" />
-          {/* <div className="flex text-2xl font-bold mt-5 flex-col justify-center items-center">
-            <span className="text-[20px]">Join the Thunder </span>
-            <div className="flex ">
-              <span>Become the Thunder</span>
-
-              <img
-                className="w-[20px]"
-                src="https://media.tenor.com/I0w9VW2gaL8AAAAi/lightning-bolt-thunder.gif"
-                alt=""
-              />
-            </div>
-          </div> */}
+ 
         </div>
         <div className="w-[100%] h-[100vh] px-2 flex items-center justify-center lg:w-[50%] bg-white flex-col ">
           {change ? (
